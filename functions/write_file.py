fbb208290c1da18230c119be3d3f6cd4807d939a
GlebCoder <gberliand@gmail.com>
2026-02-23 23:27:24 +0300

    feat: add writing to files

---
+++ functions/write_file.py
@@ -0,0 +1,24 @@
+import os
+
+
+def write_file(working_directory, file_path, content):
+    try:
+        abs_working_directory = os.path.abspath(working_directory)
+        target_file_path = os.path.normpath(os.path.join(abs_working_directory, file_path))
+        valid_file_path = os.path.commonpath([abs_working_directory, target_file_path]) == abs_working_directory
+
+        if not valid_file_path:
+            return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+
+        if os.path.isdir(target_file_path):
+            return f'Error: Cannot write to "{file_path}" as it is a directory'
+
+        parent_directory = os.path.dirname(target_file_path)
+        os.makedirs(parent_directory, exist_ok=True)
+
+        with open(target_file_path, 'w') as file:
+            file.write(content)
+
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    except Exception as e:
+        return f'Error: {e}'
